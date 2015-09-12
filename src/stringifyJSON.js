4118ffbf28650b0be7507188e6852b0b43ac7449
Charles Harrington <charles.harrington@gmail.com>
2015-09-12 16:03:50 -0400

    stringifyJSON passes all 'stringifiable objects' tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,59 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  console.log(obj);
+
+  if (typeof obj === "string") {
+    return '\"' + obj + '\"';
+  }
+
+  if (typeof obj === "number") {
+    return obj.toString();
+  }
+
+  if (obj === null) {
+    return "null";
+  }
+
+  if (typeof obj === "boolean") {
+    return obj.toString();
+  }
+
+  if (obj instanceof Array && obj.length === 0) {
+    return "[]";
+  }
+
+  if (obj instanceof Array) {
+
+    var str = "[";
+
+    for (var i = 0; i < obj.length; i++) {
+      str = str + stringifyJSON(obj[i]) + ",";
+    }
+
+    return str.slice(0, str.lastIndexOf(",")) + "]";
+
+  }
+
+  if (typeof obj === "object") {
+
+    var str = "{";
+
+    for (var thing in obj) {
+
+      str = str + stringifyJSON(thing) + ":" + stringifyJSON(obj[thing]) + ",";
+
+    }
+
+    if (str.lastIndexOf(',') > -1 ) {
+      return str.slice(0, str.lastIndexOf(",")) + "}";
+    } else  {
+      return str + "}";
+    }
+  }
+
+
+
+
 };
