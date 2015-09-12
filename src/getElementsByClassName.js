8da788d4da490b49e24250c0bcfc60cac6ff5769
Charles Harrington <charles.harrington@gmail.com>
2015-09-12 15:01:04 -0400

    Internal function to check current element for match in classList

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,29 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   // your code here
+
+  var result = [];
+
+  var currentNode = document.body;
+
+  function checkClass(current, targetClass) {
+
+    //Checks current level for a match in the class list
+    for (var i = 0; i < current.classList.length; i++) {
+      if (current.classList[i] === targetClass){
+        // PROBLEM - push is not appending the actual element;
+        // instead, it is appending something that looks like 'body.targetClassName'
+        result.push(current);
+      }
+    }
+  }
+
+  checkClass(currentNode, className);
+
+  console.log(result);
+  return result;
+
 };
+
